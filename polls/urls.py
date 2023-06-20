9355c1ce88b0ec0b7f340129d23dd414018fd727
Yurii Polych <bighunt0@gmail.com>
2023-06-20 17:36:37 +0300

    add app name to urls.py and modify path in index.html

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path('', views.index, name='index'),
-
-    # the 'name' value as called by the {% url %} template tag
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name="results"),
     path('<int:question_id>/vote/', views.vote, name="vote"),
