3af92ecf7a526929957dd495e3810d3795dfc594
Yurii Polych <bighunt0@gmail.com>
2023-06-20 17:28:24 +0300

    remove hardcoded URLs in template

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # the 'name' value as called by the {% url %} template tag
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name="results"),
     path('<int:question_id>/vote/', views.vote, name="vote"),
