cc040b7636a0ca35d0be8a24f77d64fa7be1e0cd
Yurii Polych <bighunt0@gmail.com>
2023-06-21 17:01:48 +0300

    add answer options for the question

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -8,6 +13,7 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
